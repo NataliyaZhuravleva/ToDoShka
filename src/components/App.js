3fbeec35087163e1da43e928f9cfd829bb2a8d4d
Nataliya Zhuravleva <natalindria@gmail.com>
2021-03-05 09:09:46 -0800

    add app component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+//import './App.css';
+import Places from './Places';
+
+function App() {
+  return (
+    <React.Fragment>
+      {/* <Places /> */}
+      <p>Test</p>
+    </React.Fragment>
+  );
+}
+
+export default App;
