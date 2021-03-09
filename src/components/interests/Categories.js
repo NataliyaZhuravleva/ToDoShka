7e3bb00e9b6d88e4a2d9e1ec43db0949e6525996
Nataliya Zhuravleva <natalindria@gmail.com>
2021-03-09 11:08:33 -0800

    add containers for main view

--- src/components/interests/Categories.js
+++ src/components/interests/Categories.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './categories.css';
+import Places from '../Places'
 
-function Categories(){
+function Categories() {
   return (
     <React.Fragment>
       <section className="welcomeText">
@@ -11,20 +12,26 @@ function Categories(){
       <section className="bodyText">
         Please, choose category of entertainment you are interested in.
       </section>
-      <section className="categories">
-        Categories:
-        <Router>
-          <Switch>
-            <div className='categoriesLinks'>
-              <ul id="categoriesList">
-                <li><Link to="/show" href="#">Show</Link></li>
-                <li><Link to="/show" href="#">Drink</Link></li>
-                <li><Link to="/show" href="#">Extreme</Link></li>
-                <li><Link to="/show" href="#">Family Style</Link></li>
-              </ul>
-            </div>
-          </Switch>
-        </Router>
+      <section className="userContainer">
+        <section className="categories">
+          Categories:
+          <Router>
+            <Switch>
+              <div className='categoriesLinks'>
+                <ul id="categoriesList">
+                  <li><Link to="/show" href="#">Show</Link></li>
+                  <li><Link to="/show" href="#">Drink</Link></li>
+                  <li><Link to="/show" href="#">Extreme</Link></li>
+                  <li><Link to="/show" href="#">Family Style</Link></li>
+                </ul>
+              </div>
+            </Switch>
+          </Router>
+        </section>
+        <section className="userList">
+          List
+          <Places id='places'/>
+      </section>
       </section>
     </React.Fragment>
   )
