26277689a6508d1d4e1921e820fbab9035250b2f
Nataliya Zhuravleva <natalindria@gmail.com>
2021-03-09 09:55:52 -0800

    Add buttons

--- src/components/interests/Interests.js
+++ src/components/interests/Interests.js
@@ -3,7 +3,15 @@ import React from 'react';
 function Interests(){
   return (
     <React.Fragment>
-      Test
+      <section className="welcome-text">
+        Please, choose your interest.
+      </section>
+      <section classname="buttons">
+        <button>Show</button>
+        <button>Drink</button>
+        <button>Extreme</button>
+        <button>Family Style</button>
+      </section>
     </React.Fragment>
   )
 }
