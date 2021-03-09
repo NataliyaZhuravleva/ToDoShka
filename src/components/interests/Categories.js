f5245293e6e2a645dea2c1cc0a2693c3a3ae6da4
Nataliya Zhuravleva <natalindria@gmail.com>
2021-03-09 10:54:07 -0800

    add links

--- src/components/interests/Categories.js
+++ src/components/interests/Categories.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './categories.css';
 
 function Categories(){
   return (
     <React.Fragment>
       <section className="welcomeText">
-        Welcome co ToDoShka!
+        Welcome to ToDoShka!
       </section>
       <section className="bodyText">
-        Please, choose your interest.
+        Please, choose category of entertainment you are interested in.
       </section>
-      <section className="interestsButtons">
-        <button>Show</button>
-        <button>Drink</button>
-        <button>Extreme</button>
-        <button>Family Style</button>
+      <section className="categories">
+        Categories:
+        <Router>
+          <Switch>
+            <div className='categoriesLinks'>
+              <ul id="categoriesList">
+                <li><Link to="/show" href="#">Show</Link></li>
+                <li><Link to="/show" href="#">Drink</Link></li>
+                <li><Link to="/show" href="#">Extreme</Link></li>
+                <li><Link to="/show" href="#">Family Style</Link></li>
+              </ul>
+            </div>
+          </Switch>
+        </Router>
       </section>
     </React.Fragment>
   )
