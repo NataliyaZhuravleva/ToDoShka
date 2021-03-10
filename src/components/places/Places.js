541deff3b920ce755e66abe83dca8f45cf32a194
Nataliya Zhuravleva <natalindria@gmail.com>
2021-03-10 12:54:30 -0800

    Add text to main page

--- src/components/places/Places.js
+++ src/components/places/Places.js
@@ -22,7 +22,14 @@ class Places extends React.Component {
     } else if (isLoading) {
       return <React.Fragment>
         <section className="bodyText">
-          Please, choose a category you are interested in.
+        <p>When you are planning your trip, you are trying to find Things ToDo internet pages about a particular area. However, 
+        all people are different with different interests. One person would like to go to Las Vegas for walking and drinking. 
+        Another one would go to Las Vegas Area for a rock climbing in the Red Rocks National Park. Families with kids would love 
+        to know about kid’s entertainment, like a roller coaster.</p> 
+        <p>ToDoSka is the app creating customized Things ToDo List considering your interests and giving you a 
+          list for your personal purposes.</p>
+        <p>For now, Todoshka considers trips to Las Vegas, NV only. In the future, we are planning to expand trip locations.</p>  
+        <p>Plase, choose a category of your interests.</p>
       </section>
 
       </React.Fragment>;
